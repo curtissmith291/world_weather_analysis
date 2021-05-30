3240ff97070a4184ce4d88ac3df1afca9fb3273b
curtissmith291 <82423123+curtissmith291@users.noreply.github.com>
2021-05-29 20:41:12 -0500

    added direction maps for itinerary and supporting images

--- test.py
+++
@@ -1,7 +0,0 @@
-import os
-
-body_Str="Body of Dialog"
-
-title_Str="Title"
-
-os.system("""osascript -e \'Tell application \"System Events\" \\to display dialog \""+body_Str+"\" with title \""+title_Str+"\"\'""")
